bdf493fbab7795f76bcd1910598874e5673cc993
MaxFerreira <max.alexxandre@gmail.com>
2023-01-31 19:33:54 -0300

    Criando migrations restantes e implementando a model de fornecedor

--- database/migrations/2023_01_26_222828_create_unidades_table.php
+++ database/migrations/2023_01_26_222828_create_unidades_table.php
@@ -15,7 +15,19 @@ class CreateUnidadesTable extends Migration
     {
         Schema::create('unidades', function (Blueprint $table) {
             $table->id();
+            $table->string('unidade', 5);
+            $table->string('descricao', 30);
             $table->timestamps();
+        });
+
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->references('id')->on('unidades');
+        });
+
+        Schema::table('produto_detalhes', function (Blueprint $table) {
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->references('id')->on('unidades');
         });
     }
 
@@ -26,6 +38,16 @@ class CreateUnidadesTable extends Migration
      */
     public function down()
     {
+        Schema::table('produto_detalhes', function (Blueprint $table) {
+            $table->dropForeign('produto_detalhes_unidade_id_foreign');
+            $table->dropColumn('unidade_id');
+        });
+
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->dropForeign('produtos_unidade_id_foreign');
+            $table->dropColumn('unidade_id');
+        });
+
         Schema::dropIfExists('unidades');
     }
 }
