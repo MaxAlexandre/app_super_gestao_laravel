a34b367f0395c6073ef833c10e1f1e91edd6a574
MaxFerreira <max.alexxandre@gmail.com>
2023-03-02 20:03:14 -0300

    Criando validações do formulario e utilizando helper old

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato(){
-        var_dump($_POST);
+    public function contato(Request $request)
+    {
+//        $contato = new SiteContato();
+//        $contato->nome = $request->input('nome');
+//        $contato->telefone = $request->input('telefone');
+//        $contato->email = $request->input('email');
+//        $contato->motivo_contato = $request->input('motivo_contato');
+//        $contato->mensagem = $request->input('mensagem');
+//        $contato->save();
+
+//        $contato = new SiteContato();
+//        $contato->fill($request->all());
+//        $contato->save();
+
         return view('site.contato');
+    }
+
+    public function salvar(Request $request)
+    {
+        //validando dados vindos do request
+        $request->validate([
+            'nome' => 'required|min:3|max:40', //nomes com no máximo 3 caracteres e no máximo 40
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:2000'
+        ]);
+//        SiteContato::create($request->all());
     }
 }
