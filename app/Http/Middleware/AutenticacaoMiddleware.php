e5f250205c4bd15ca39452b104901f10b8150b4e
MaxFerreira <max.alexxandre@gmail.com>
2023-04-06 19:39:25 -0300

    Implementando Middleware de autenticação e Tela de Login

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -3,22 +3,38 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\View\Compilers\Concerns\CompilesEchos;
 
 class AutenticacaoMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $metodo_autenticacao, $perfil)
     {
         //verifica se o usuário possui acesso a rota
-        if(true){
+        echo $metodo_autenticacao .' - '.$perfil.'<br>';
+        if ($metodo_autenticacao == 'padrao') {
+            echo 'Verificar usuário e senha no banco de dados'.$perfil.'<br>';
+        }
+
+        if ($metodo_autenticacao == 'ldap') {
+            echo 'Verificar usuário e senha no AD'.$perfil.'<br>';
+        }
+
+        if ($perfil == 'visitante') {
+            echo 'Exibir apenas alguns recursos';
+        } else {
+            echo 'Carregar o perfil do banco de dados';
+        }
+
+        if (true) {
             return $next($request);
-        }else{
+        } else {
             return Response('Acesso Negado! Rota exige autenticação!!!');
         }
     }
--- app/Http/Middleware/LogAcessoMiddleware.php
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -10,8 +10,8 @@ class LogAcessoMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
@@ -19,7 +19,9 @@ class LogAcessoMiddleware
         $ip = $request->server->get('REMOTE_ADDR');
         $rota = $request->getRequestUri();
         LogAcesso::create(['log' => "Ip $ip xyz requisitou a rota $rota"]);
-        return $next($request);
-
+        //return $next($request);
+        $resposta = $next($request);
+        $resposta->setStatusCode(201, 'O status da resposta e o texto foram modificados!!!');
+        return $resposta;
     }
 }
