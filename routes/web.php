64dfa3b6b6bf4e2751ff13970e55508be0dffefd
MaxFerreira <max.alexxandre@gmail.com>
2023-11-27 00:08:26 -0300

    Relacionamentos 1 para 1 Eloquent ORM

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,11 @@ Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(functi
     Route::get('/fornecedor/editar/{id}/{msg?}', 'FornecedorController@editar')->name('app.fornecedor.editar');
     Route::get('/fornecedor/excluir/{id}', 'FornecedorController@excluir')->name('app.fornecedor.excluir');
 
-
+    //produtos
     Route::resource('produto','ProdutoController');
+
+    //produtos-detalhes
+    Route::resource('produto-detalhe','ProdutoDetalheController');
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
