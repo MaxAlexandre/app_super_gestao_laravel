cd65deb2306e3716cb766d6b4522ce067bbeed5c
MaxFerreira <max.alexxandre@gmail.com>
2023-12-06 00:29:37 -0300

    Relacionamento N para N, Clientes, pedidos, pedido_produtos

--- resources/views/app/produto/_components/form_create_edit.blade.php
+++ resources/views/app/produto/_components/form_create_edit.blade.php
@@ -6,12 +6,24 @@
             <form method="post" action="{{ route('produto.store') }}">
                 @csrf
                 @endif
-                <input type="text" name="nome" value="{{ $produto->nome ?? old('nome')}}" placeholder="Nome" class="borda-preta">
+                <select name="fornecedor_id">
+                    <option value="">-- Selecione um Fornecedor --</option>
+
+                    @foreach($fornecedores as $fornecedor)
+                        <option
+                            value="{{$fornecedor->id}}" {{($produto->fornecedor_id ?? old('fornecedor_id')) === $fornecedor->id ? 'selected' : '' }} >{{$fornecedor->nome}}</option>
+                    @endforeach
+                </select>
+                {{ $errors->has('fornecedor_id') ? $errors->first('fornecedor_id') : '' }}
+                <input type="text" name="nome" value="{{ $produto->nome ?? old('nome')}}" placeholder="Nome"
+                       class="borda-preta">
                 {{ $errors->has('nome') ? $errors->first('nome') : '' }}
-                <input type="text" name="descricao" value="{{$produto->descricao ?? old('descricao')}}" placeholder="Descrição"
+                <input type="text" name="descricao" value="{{$produto->descricao ?? old('descricao')}}"
+                       placeholder="Descrição"
                        class="borda-preta">
                 {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
-                <input type="text" name="peso" value="{{$produto->peso ?? old('peso')}}" placeholder="Peso" class="borda-preta">
+                <input type="text" name="peso" value="{{$produto->peso ?? old('peso')}}" placeholder="Peso"
+                       class="borda-preta">
                 {{ $errors->has('peso') ? $errors->first('peso') : '' }}
                 <select name="unidade_id">
                     <option value="">-- Selecione a Unidade de Medida --</option>
